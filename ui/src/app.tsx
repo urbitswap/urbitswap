da03494aeefce2069b78b1ab7249f39ad01a3545
Sidnym Ladrut <sidnym@ladrut.xyz>
2023-09-12 22:54:47 +0000

    extend static collection example to enable listing and de-listing items

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -2,13 +2,21 @@ import React, { useEffect, useState } from 'react';
 import Urbit from '@urbit/http-api';
 import { ethers } from 'ethers';
 import { createRaribleSdk } from '@rarible/sdk';
+import { Blockchain } from '@rarible/api-client';
+import { toContractAddress, toItemId, toOrderId, toUnionAddress } from '@rarible/types';
+
 import type {
   Collection as RaribleCollection,
   Item as RaribleItem,
   Items as RaribleItems,
   MetaContent as RaribleMetaContent,
 } from '@rarible/api-client';
-import { toUnionAddress } from '@rarible/types';
+import type {
+  IBlockchainTransaction as RaribleTransaction,
+} from '@rarible/sdk-transaction';
+import type {
+  OrderId as RaribleOrderId,
+} from '@rarible/types';
 
 const api = new Urbit('', '', window.desk);
 api.ship = window.ship;
@@ -26,26 +34,32 @@ const rsdk = createRaribleSdk(eth, "testnet", {
 });
 
 export function App() {
-  const collectionAddress = "ETHEREUM:0xa144C7E81398B90680bBb34320e062f4bFE37564";
+  const collectionAddress = toContractAddress(
+    "ETHEREUM:0xa144C7E81398B90680bBb34320e062f4bFE37564"
+  );
+  const usdcAddress = toContractAddress(
+    "ETHEREUM:0x9f853f3B6a9dD425F8Cf73eF5B90e8eBf412317b"
+  );
 
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [collectionName, setCollectionName] = useState<string>("");
   const [collectionItems, setCollectionItems] = useState<RaribleItem[]>([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const colArgs = {collection: collectionAddress};
-      const col: RaribleCollection =
-        await rsdk.apis.collection.getCollectionById(colArgs);
-      const colItems: RaribleItems =
-        await rsdk.apis.item.getItemsByCollection(colArgs);
-
-      setCollectionName(col.name);
-      setCollectionItems(colItems.items);
-      setIsLoading(false);
-    };
-    fetchData();
-  }, []);
+    if (isLoading) {
+      Promise.all([
+        rsdk.apis.collection.getCollectionById({collection: collectionAddress}),
+        rsdk.apis.item.getItemsByCollection({collection: collectionAddress}),
+      ]).then(([col, colItems]: [RaribleCollection, RaribleItems]) => {
+        setCollectionName(col.name);
+        setCollectionItems(colItems.items);
+        setIsLoading(false);
+      }, (reason: any) => {
+        console.log("*********** COLLECTION QUERY FAILED ***********");
+        setIsLoading(false);
+      });
+    }
+  }, [isLoading]);
 
   return (
     <main>
@@ -60,7 +74,39 @@ export function App() {
               justify-center sm:grid-cols-[repeat(auto-fit,minmax(auto,200px))]
             `}>
               {collectionItems.map((collectionItem: RaribleItem) => (
-                <div className="flex flex-col justify-center" key={collectionItem.tokenId}>
+                <div key={collectionItem.tokenId}
+                  className="flex flex-col justify-center hover:cursor-pointer hover:border-2"
+                  onClick={() => {
+                    if (collectionItem?.bestSellOrder === undefined) {
+                      rsdk.order.sell({
+                        itemId: toItemId(collectionItem.id),
+                        amount: 1,
+                        price: "0.0005",
+                        currency: {
+                          "@type": "ETH",
+                          "blockchain": Blockchain.ETHEREUM,
+                        },
+                        expirationDate: new Date(Date.now() + 60 * 60 * 1000),
+                      }).then((orderId: RaribleOrderId) => {
+                        console.log("*************** ORDER SUCCEEDED ***************");
+                        setIsLoading(true);
+                      }, (reason: any) => {
+                        console.log("**************** ORDER FAILED ****************");
+                        console.log(reason);
+                      });
+                    } else {
+                      rsdk.order.cancel({
+                        orderId: toOrderId(collectionItem.bestSellOrder.id),
+                      }).then((orderTxn: RaribleTransaction) => {
+                        console.log("*************** CANCEL SUCCEEDED ***************");
+                        setIsLoading(true);
+                      }, (reason: any) => {
+                        console.log("**************** CANCEL FAILED ****************");
+                        console.log(reason);
+                      });
+                    }
+                  }}
+                >
                   <h3 className="text-lg text-center font-semibold">
                     {collectionItem.meta?.name ?? "<Unknown Collection>"}
                   </h3>
@@ -69,6 +115,13 @@ export function App() {
                       entry["@type"] === "IMAGE"
                     )) ?? {})?.url
                   } />
+                  <p className="text-sm text-center">
+                    {(collectionItem?.bestSellOrder === undefined) ? (
+                      "Unlisted"
+                    ) : (
+                      `${collectionItem.bestSellOrder.makePrice} ETH`
+                    )}
+                  </p>
                 </div>
               ))}
             </div>
