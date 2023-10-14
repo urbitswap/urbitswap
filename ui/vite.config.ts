bcefb39fee470c51c3dbe437b4ac6790b6eda441
Sidnym Ladrut <sidnym@ladrut.xyz>
2023-10-14 03:11:19 +0000

    improve page load times via manual chunking and unused dep removal

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -11,8 +11,51 @@ export default ({ mode }) => {
   const SHIP_URL = process.env.SHIP_URL || process.env.VITE_SHIP_URL || 'http://localhost:8080';
   console.log(SHIP_URL);
 
+  const rollupOptions = {
+    output: {
+      manualChunks: (id: string): string | void => {
+        if (id.includes('@radix-ui')) {
+          return '@radix-ui';
+        } if (id.includes('@urbit') || id.includes('@tlon')) {
+          return '@urbit';
+        } if (id.includes('react-select')) {
+          return 'react-select';
+        } if (id.includes('react-datetime-picker')) {
+          return 'react-datetime-picker';
+        } if (id.includes('@ethersproject')) {
+          return '@ethersproject';
+        } if (id.includes('ethers')) {
+          return 'ethers';
+        } if (id.includes('@rarible/sdk')) {
+          return '@rarible/sdk';
+        } if (id.match(/@rarible\/flow/)) {
+          return '@rarible/flow';
+        } if (id.match(/@rarible\/tezos/)) {
+          return '@rarible/tezos';
+        } if (id.match(/@rarible\/solana/)) {
+          return '@rarible/solana';
+        } if (id.match(/@rarible\/[^\/]*ethereum[^\/]*/)) {
+          return '@rarible/ethereum';
+        } if (id.includes('@rarible')) {
+          return '@rarible';
+        }
+      },
+      // manualChunks: {
+      //   '@urbit/api': ['@urbit/api'],
+      //   '@urbit/http-api': ['@urbit/http-api'],
+      //   '@tlon/sigil-js': ['@tlon/sigil-js'],
+      //   'react-select': ['react-select'],
+      //   'react-datetime-picker': ['react-datetime-picker'],
+      //   'ethers': ['ethers'],
+      //   '@radix-ui/react-dialog': ['@radix-ui/react-dialog'],
+      //   '@radix-ui/react-dropdown-menu': ['@radix-ui/react-dropdown-menu'],
+      //   '@rarible/sdk': ['@rarible/sdk'],
+      // },
+    },
+  };
+
   return defineConfig({
-    build: { sourcemap: false },
+    build: { sourcemap: false, rollupOptions },
     plugins: [urbitPlugin({ base: 'vcc-trade', target: SHIP_URL, secure: false }), reactRefresh()],
     server: { host: 'localhost', port: 3000 },
     resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
@@ -20,10 +63,10 @@ export default ({ mode }) => {
     // https://github.com/remorses/esbuild-plugins/issues/14#issuecomment-1437893495
     optimizeDeps: {
       esbuildOptions: {
-        define: {global: 'globalThis'},
+        define: { global: 'globalThis' },
         plugins: [
           NodeModulesPolyfills(),
-          GlobalsPolyfills({process: true, buffer: true}),
+          GlobalsPolyfills({ process: true, buffer: true }),
         ],
       },
     },
