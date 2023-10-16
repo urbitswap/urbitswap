c733119e225e7046061d538020c1c3a4a6ebf04d
Sidnym Ladrut <sidnym@ladrut.xyz>
2023-10-16 19:27:00 +0000

    fix issues with globbing for production/deployment

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -14,8 +14,7 @@ export default ({ mode }) => {
     plugins: [
       urbitPlugin({ base: 'vcc-trade', target: SHIP_URL, secure: false }),
       reactRefresh(),
-      // Current Solution: https://stackoverflow.com/a/77153849
-      // Old Solution: https://stackoverflow.com/a/69021714
+      // https://stackoverflow.com/a/77153849, https://stackoverflow.com/a/69021714
       // https://github.com/remorses/esbuild-plugins/issues/14#issuecomment-1437893495
       nodePolyfills(),
     ],
@@ -30,43 +29,43 @@ export default ({ mode }) => {
     },
     build: {
       sourcemap: false,
-      // rollupOptions: {
-      //   // output: {
-      //   //   manualChunks: {
-      //   //     '@urbit/api': ['@urbit/api'],
-      //   //     '@urbit/http-api': ['@urbit/http-api'],
-      //   //     '@tlon/sigil-js': ['@tlon/sigil-js'],
-      //   //     'react-select': ['react-select'],
-      //   //     'react-datetime-picker': ['react-datetime-picker'],
-      //   //     'ethers': ['ethers'],
-      //   //     '@radix-ui/react-dialog': ['@radix-ui/react-dialog'],
-      //   //     '@radix-ui/react-dropdown-menu': ['@radix-ui/react-dropdown-menu'],
-      //   //     '@rarible/sdk': ['@rarible/sdk'],
-      //   //   },
-      //   // //   manualChunks: (id: string): string | void => {
-      //   // //     if (id.includes('@radix-ui')) {
-      //   // //       return '@radix-ui';
-      //   // //     } if (id.includes('@urbit') || id.includes('@tlon')) {
-      //   // //       return '@urbit';
-      //   // //     } if (id.includes('react-select')) {
-      //   // //       return 'react-select';
-      //   // //     } if (id.includes('react-datetime-picker')) {
-      //   // //       return 'react-datetime-picker';
-      //   // //     // TODO: May need to combine with '@rarible'
-      //   // //     } if (id.includes('ethers')) {
-      //   // //       return 'ethers';
-      //   // //     } if (id.match(/@rarible\/flow/)) {
-      //   // //       return '@rarible/flow';
-      //   // //     } if (id.match(/@rarible\/tezos/)) {
-      //   // //       return '@rarible/tezos';
-      //   // //     } if (id.match(/@rarible\/solana/)) {
-      //   // //       return '@rarible/solana';
-      //   // //     } if (id.includes('@rarible')) {
-      //   // //       return '@rarible/sdk';
-      //   // //     }
-      //   // //   },
-      //   // },
-      // },
+      commonjsOptions: {
+        // Needed to build web3js deps w/ mixed import/require statements
+        // https://github.com/rollup/plugins/tree/master/packages/commonjs#transformmixedesmodules
+        transformMixedEsModules: true,
+      },
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            'ethers': ['ethers'],
+            'urbit': ['@urbit/api', '@urbit/http-api', '@tlon/sigil-js'],
+            'react-ui': ['react-select', 'react-datetime-picker'],
+            'radix-ui': ['@radix-ui/react-dialog', '@radix-ui/react-dropdown-menu'],
+            '@rarible/sdk': ['@rarible/sdk'],
+          },
+          // FIXME: The following has better size chunks, but splitting
+          // up '@rarible/sdk' doesn't seem to work very easily (even if
+          // only splitting off unused components, e.g. solana and tezos).
+          // manualChunks: (id: string): string | void => {
+          //   if (id.includes('@radix-ui')) {
+          //     return 'radix-ui';
+          //   } if (id.includes('@urbit') || id.includes('@tlon')) {
+          //     return 'urbit';
+          //   } if (id.includes('react-datetime-picker') ||
+          //         id.includes('react-select')) {
+          //     return 'react-ui';
+          //   } if (id.match(/@rarible\/flow/)) {
+          //     return '@rarible/flow';
+          //   } if (id.match(/@rarible\/tezos/)) {
+          //     return '@rarible/tezos';
+          //   } if (id.match(/@rarible\/solana/)) {
+          //     return '@rarible/solana';
+          //   } if (id.includes('@rarible')) {
+          //     return '@rarible/sdk';
+          //   }
+          // },
+        },
+      },
     },
   });
 };
